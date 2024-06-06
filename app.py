9d87d865f34edde029c0132ac11c7b91507a4428
diapolar <163065672+diapolar@users.noreply.github.com>
2024-06-06 04:17:45 +0000

    Second commit

--- app.py
+++ app.py
@@ -2,6 +2,7 @@
 #print('hello world')
 
 #Juego de piedra, papel o tijera con la computadora
+# Dar la opción de terminar el juego o seguir jugando y dar un resumen de número de partidas ganadas por el usuario y la computadora
 # Path: app.py
 import random
 # Definir las opciones
@@ -24,4 +25,39 @@ elif usuario == 'tijera' and computadora == 'papel':
     print('Gana el usuario!')
 else:
     print('Gana la computadora!')   
-    
+
+# Preguntar al usuario si desea seguir jugando
+seguir = input('Desea seguir jugando? (s/n): ')
+# Inicializar los contadores
+usuario_gana = 0
+computadora_gana = 0
+# Mientras el usuario quiera seguir jugando
+while seguir == 's':
+    # Elegir una opción aleatoria
+    computadora = random.choice(opciones)
+    # Preguntar al usuario
+    usuario = input('Elige piedra, papel o tijera: ')
+    # Mostrar las opciones elegidas
+    print(f'Usuario: {usuario}')
+    print(f'Computadora: {computadora}')
+    # Determinar el ganador
+    if usuario == computadora:
+        print('Empate!')
+    elif usuario == 'piedra' and computadora == 'tijera':
+        print('Gana el usuario!')
+        usuario_gana += 1
+    elif usuario == 'papel' and computadora == 'piedra':
+        print('Gana el usuario!')
+        usuario_gana += 1
+    elif usuario == 'tijera' and computadora == 'papel':
+        print('Gana el usuario!')
+        usuario_gana += 1
+    else:
+        print('Gana la computadora!')
+        computadora_gana += 1
+    # Preguntar al usuario si desea seguir jugando
+    seguir = input('Desea seguir jugando? (s/n): ')
+# Mostrar el resumen de partidas ganadas    
+print(f'Usuario: {usuario_gana} partidas ganadas')
+print(f'Computadora: {computadora_gana} partidas ganadas')
+print('Fin del juego!')
